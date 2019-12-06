d0f70df7f089a6a87ac419e3df32b8ee7b0a3f0e
fiopwk <flopwiki@gmail.com>
2019-12-06 16:55:43 -0500

    index.js: added resize window.eventListener to nav

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+const main_nav = document.querySelector('.main-navigation')
+window.addEventListener('resize', (event) => {
+    main_nav.style.backgroundColor = '#1222';
+
+})
