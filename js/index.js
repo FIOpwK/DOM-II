15a9b62f9b31c0907632886eb0fc399fbfe01158
fiopwk <flopwiki@gmail.com>
2019-12-06 19:24:28 -0500

    index.js: added new image for dbclick event, README: checkbox event

--- js/index.js
+++ js/index.js
@@ -1,9 +1,14 @@
 // Your code goes here
+// const main_html = document.querySelector('html');
+// main_html.style.backgroundColor = 'red';
 const main_nav = document.querySelector('.main-navigation')
 window.addEventListener('resize', (event) => {
     main_nav.style.backgroundColor = '#1222';
 
 })
 
-const main_html = document.querySelector('html');
-main_html.style.backgroundColor = 'red';
+
+const all_images = document.querySelectorAll('img');
+all_images[0].addEventListener('dblclick', (event) => {
+    all_images[0].setAttribute('src', 'img/13178635_1082197168511196_4041460445760705289_n.jpg')
+})
