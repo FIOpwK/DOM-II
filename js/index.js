a88f6b9e9a8afa0bc38b1e5f84b42bbe6e32eb65
fiopwk <flopwiki@gmail.com>
2019-12-07 22:18:50 -0500

    index.js: added console.log to event with number

--- js/index.js
+++ js/index.js
@@ -1,13 +1,20 @@
 // [1]
-window.addEventListener('scroll', (scroll) => {console.log('scroll')})
+window.addEventListener('scroll', (scroll) => {console.log('scroll [2]')})
 // [2]
-window.addEventListener('load', (load) => {console.log('load')})
+window.addEventListener('load', (load) => {
+    for(let i=0; i<all_buttons.length; i++) {
+        all_buttons[i].textContent = 'Fun Button';
+    }
+    
+    console.log('load [1]')
+})
 // [3]
 window.addEventListener('resize', (resize) => {
     for(let i=0; i<all_buttons.length; i++) {
         all_buttons[i].style.backgroundColor = 'red';
     }
-    console.log('resize')})
+    console.log('resize [3]')
+})
 
 // Grab Elements
 const nav = document.querySelectorAll('.nav', '.nav-link')
@@ -21,38 +28,38 @@ const content_pick = document.querySelectorAll('.content-pick', '.destination')
 // :::: NAV :::: 
 // [4]
 nav[0].addEventListener('mouseover', (mouseover) => {
-    console.log('mouseover')
+    console.log('mouseover [4]')
 })  
 
 // :::: IMAGES ::::
 // [5]
 all_images[0].addEventListener('mouseenter', (mouseenter) => {
-    console.log('mouse enter')
+    console.log('mouse enter [5]')
 })
 // [6]
 all_images[0].addEventListener('mouseleave', (mouseleave) => {
-    console.log('mouse leave')
+    console.log('mouse leave [6]')
 })
 // [7]
 all_images[0].addEventListener('dblclick', (dblclick) => {
     all_images[0].setAttribute('src', 'img/13178635_1082197168511196_4041460445760705289_n.jpg')
-    console.log('dblclick')
+    console.log('dblclick [7]')
 
 })
 // [8]
 all_images[0].addEventListener('drag', (drag) => {
     all_images[0].style.borderRadius = '40rem';
-    console.log("drag")
+    console.log('drag [8]')
 })
 // [9]
 all_images[0].addEventListener('dragleave', (dragleave) => {
     all_images[0].style.borderRadius = '';
-    console.log('dragleave')
+    console.log('dragleave [9]')
 })
 
 
 //  :::: BUTTONS ::::
 // [10]
 all_buttons[0].addEventListener('click', (click) => {
-    console.log('click')
+    console.log('click [10]')
 })
