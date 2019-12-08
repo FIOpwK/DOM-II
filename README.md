9196e6cac65e678f13382eb08679d0789ecdc492
fiopwk <flopwiki@gmail.com>
2019-12-07 22:01:45 -0500

    index.js: refactor code, added drag,drop,dragleave events

--- README.md
+++ README.md
@@ -46,7 +46,7 @@ Fun Bus wants you to make their site more interactive. They are relying on you t
 	* [ ] `wheel`
 	* [ ] `drag / drop`
 	* [ ] `focus`
-	* [ ] `scroll`
+	* [x] `scroll`
 	* [ ] `select`
 
 Using the 10 unique events, find ways to update the DOM in creative ways. For example you could change colors, animate objects, remove objects, etc.
--- js/index.js
+++ js/index.js
@@ -1,111 +1,58 @@
-// Your code goes here
-
-const content = document.querySelector('.content-pick')
-content.addEventListener('click', (event) => {
-    content.style.backgroundColor = 'red'
-    event.preventDefault();
-    
-})
-
-
-
-
-// mouseenter
-const nav = document.querySelectorAll('.nav', '.nav-link')
-nav[0].addEventListener('mouseenter', (event) => {
-    
-    nav[0].style.border = '1px dotted red';
-    event.preventDefault();
-
-    console.log(nav[0])
-})
-
-
-// mouseleave
-nav[0].addEventListener('mouseleave', (event) => {
-    nav[0].style.border = '';
-    
-    main_nav.style.backgroundColor = '';
-    main_nav.style.color = 'black';
-    // event.preventDefault();
-    
-})
-
-
-
-
-// load
-window.addEventListener('load', (event) => {
-    for (let i=0; i< all_buttons.length; i++) {
+// [1]
+window.addEventListener('scroll', (scroll) => {console.log('scroll')})
+// [2]
+window.addEventListener('load', (load) => {console.log('load')})
+// [3]
+window.addEventListener('resize', (resize) => {
+    for(let i=0; i<all_buttons.length; i++) {
         all_buttons[i].style.backgroundColor = 'red';
     }
-    
-})
+    console.log('resize')})
 
-const main_html = document.querySelectorAll('html')
-
-const main_body = document.querySelector('body')
-main_body.style.overflow = 'scroll';
-
-
-// resize
-const main_nav = document.querySelector('.main-navigation')
-window.addEventListener('resize', (event) => {
-    main_nav.style.backgroundColor = '#1222';
-    
-})
-
-main_nav.addEventListener('mouseenter', (event) => {
-    main_nav.style.color = 'green';
-})
-// dblclick
+// Grab Elements
+const nav = document.querySelectorAll('.nav', '.nav-link')
 const all_images = document.querySelectorAll('img')
-all_images[0].addEventListener('dblclick', (event) => {
-    all_images[0].setAttribute('src', 'img/13178635_1082197168511196_4041460445760705289_n.jpg')
-    all_images[1].setAttribute('src', 'img/explora_map.jpg')
-    all_images[2].setAttribute('src', 'img/disney_adventure.jpg')
-    all_images[3].setAttribute('src', 'img/black_perl.jpg')
-    all_images[0].style.height = '30rem';
-    main_body.style.backgroundColor = 'black';
-    main_body.style.color = 'white';
-    main_body.style.fontFamily = 'cursive';
-    
-    for (let i=0; i<all_buttons.length; i++) {
-        all_buttons[i].style.backgroundColor = 'limegreen';
-        
-    }
-    
-})
-
-// mouseover
-all_images[0].addEventListener('mouseover', (event) => {
-    all_images[1].style.borderRadius = '40rem';
-    all_images[2].style.borderRadius = '40rem';
-    all_images[3].style.borderRadius = '40rem';
-    
-})
-
-
-
-// mouseout
-all_images[0].addEventListener('mouseout', (event) => {
-    all_images[0].style.borderRadius = '';
-    all_images[1].style.borderRadius = '';
-    all_images[2].style.borderRadius = '';
-    all_images[3].style.borderRadius = '';
-})
-
-// click
 const all_buttons = document.querySelectorAll('.btn')
-all_buttons[0].addEventListener('click', (event) => {
-    all_buttons[0].style.backgroundColor =  'dodgerblue';
-    all_buttons[0].style.color = 'white';
-    content.style.backgroundColor = '';
-}) 
+const content_pick = document.querySelectorAll('.content-pick', '.destination')
 
-const destination = document.querySelectorAll('.destination')
 
-// mouseenter
-destination[0].addEventListener('mouseenter', (event) => {
-    all_buttons[1].style.backgroundColor = 'black';
+// Document Events
+
+// :::: NAV :::: 
+// [4]
+nav[0].addEventListener('mouseover', (mouseover) => {
+    console.log('mouseover')
+})  
+
+// :::: IMAGES ::::
+// [5]
+all_images[0].addEventListener('mouseenter', (mouseenter) => {
+    console.log('mouse enter')
+})
+// [6]
+all_images[0].addEventListener('mouseleave', (mouseleave) => {
+    console.log('mouse leave')
+})
+// [7]
+all_images[0].addEventListener('dblclick', (dblclick) => {
+    all_images[0].setAttribute('src', 'img/13178635_1082197168511196_4041460445760705289_n.jpg')
+    console.log('dblclick')
+
+})
+// [8]
+all_images[0].addEventListener('drag', (drag) => {
+    all_images[0].style.borderRadius = '40rem';
+    console.log("drag")
+})
+// [9]
+all_images[0].addEventListener('dragleave', (dragleave) => {
+    all_images[0].style.borderRadius = '';
+    console.log('dragleave')
+})
+
+
+//  :::: BUTTONS ::::
+// [10]
+all_buttons[0].addEventListener('click', (click) => {
+    console.log('click')
 })
